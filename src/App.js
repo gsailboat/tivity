1a3261c59c2744c199c0bfd48d973096311b8e50
Greg Selbo <gregselbo@Gregs-MBP.hsd1.ca.comcast.net>
2018-09-14 16:25:46 -0700

    App.js cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Extend from './components/Extend'
-//import ToDoList from './components/ToDoList';
-
-//import CssBaseline from '@material-ui/core/CssBaseline';
-//import Journal from './components/Journal';
-//import Tasks from './components/Tasks';
-//import Goals from './components/Goals';
-
-//const total = 0;
 
 class App extends Component {
   render() {
